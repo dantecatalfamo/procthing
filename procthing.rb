01518ab176687a622f499f1e4b0bfee8cad015ab
Dante Catalfamo <dante.catalfamo@gmail.com>
2021-08-12 23:46:34 -0400

    Change how options are passed to ProcTree, move kproc to method

--- procthing.rb
+++ procthing.rb
@@ -16,6 +16,7 @@ class ProcNode
   end
 end
 
+# Class used to create and render the process tree
 class ProcTree
   COLORS = %i[red green yellow bright_blue magenta cyan white].freeze
   VERTICAL = '│'
@@ -25,10 +26,11 @@ class ProcTree
   CONNECTOR = '├'
   CONNECTOR_LAST = '└'
 
-  attr_accessor :pastel
+  attr_accessor :pastel, :options
   attr_reader :tree, :procs
 
-  def initialize
+  def initialize(options = {})
+    @options = options
     @procs = Sys::ProcTable.ps(smaps: false)
     @tree = gen_tree(@procs)
     @pastel = Pastel.new
@@ -38,15 +40,19 @@ class ProcTree
     COLORS[depth % COLORS.length]
   end
 
+  def kproc
+    kproc = Struct::ProcTableStruct.new
+    kproc.name = 'Kernel'
+    kproc.comm = Gem::Platform.local.os
+    kproc.pid = 0
+    kproc
+  end
+
   def gen_tree(procs)
     tree = procs.each_with_object({}) do |prc, h|
       h[prc.pid] = ProcNode.new(prc)
     end
 
-    kproc = Struct::ProcTableStruct.new
-    kproc.name = 'Kernel'
-    kproc.comm = Gem::Platform.local.os
-    kproc.pid = 0
     tree[0] = ProcNode.new(kproc)
 
     tree.each_pair do |pid, prtr|
@@ -57,7 +63,7 @@ class ProcTree
     end
   end
 
-  def print_node(prc, depth, last, depth_map, options)
+  def print_node(prc, depth, last, depth_map)
     pipes = []
     (depth - 1).times do |i|
       if depth_map[i + 1]
@@ -73,23 +79,23 @@ class ProcTree
       pipes << @pastel.send(depth_color(depth - 1), last ? CONNECTOR_LAST : CONNECTOR)
       pipes << @pastel.send(depth_color(depth), prc.children.empty? ? HORIZONTAL : HORIZONTAL_CHILDREN)
     end
-    pid_str = "[#{prc.proc.pid}] " if options[:pid]
-    comm_str = " (#{prc.proc.comm})" if options[:comm]
-    cmd_str = " [#{prc.proc.cmdline}]" if options[:cmd]
+    pid_str = "[#{prc.proc.pid}] " if @options[:pid]
+    comm_str = " (#{prc.proc.comm})" if @options[:comm]
+    cmd_str = " [#{prc.proc.cmdline}]" if @options[:cmd]
     puts "#{pipes.join}#{pid_str}#{prc.proc.name}#{comm_str}#{cmd_str}"
     prc.children.each do |chld|
-      print_node(chld, depth + 1, chld == prc.children.last, depth_map.clone << !last, options)
+      print_node(chld, depth + 1, chld == prc.children.last, depth_map.clone << !last)
     end
   end
 
-  def print_tree(options = {})
-    print_node(@tree[0], 0, false, [], options)
+  def print_tree
+    print_node(@tree[0], 0, false, [])
   end
 end
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{File.basename $0} [options]"
+  opts.banner = "Usage: #{File.basename $PROGRAM_NAME} [options]"
 
   opts.on('-p', '--pid', 'Display process ID') do
     options[:pid] = true
@@ -102,4 +108,4 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-ProcTree.new.print_tree(options)
+ProcTree.new(options).print_tree
